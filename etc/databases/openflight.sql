220bf29aa9ab4f4c620643927129a2bf8e7d3b58
erouresvives <51374711+erouresvives@users.noreply.github.com>
2021-04-28 21:00:19 +0200

    Correccion openflight.sql

--- etc/databases/openflight.sql
+++ etc/databases/openflight.sql
@@ -2,8 +2,7 @@ DROP
 DATABASE IF EXISTS `open_flight`;
 CREATE
 DATABASE IF NOT EXISTS `open_flight`;
-USE
-`open_flight`;
+USE `open_flight`;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`
@@ -20,7 +19,7 @@ DROP TABLE IF EXISTS `flight`;
 CREATE TABLE `flight`
 (
     `Id`             CHAR(36) NOT NULL,
-    `Origin  `       TEXT     NOT NULL,
+    `Origin`       TEXT     NOT NULL,
     `Destination`    TEXT     NOT NULL,
     `Flight-hours`   INT      NOT NULL,
     `Price`          INT      NOT NULL,
