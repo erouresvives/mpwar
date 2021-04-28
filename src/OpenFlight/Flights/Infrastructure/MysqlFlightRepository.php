bbe6731907e40b8c86eac0a7c89d703c7346d682
erouresvives <51374711+erouresvives@users.noreply.github.com>
2021-04-28 21:42:31 +0200

    correcion path infrastructure + insercion Id

--- src/OpenFlight/Flights/Infrastructure/MysqlFlightRepository.php
+++ src/OpenFlight/Flights/Infrastructure/MysqlFlightRepository.php
@@ -3,13 +3,12 @@
 declare(strict_types=1);
 
 
-namespace CodelyTv\OpenFlight\Users\Infrastructure;
+namespace CodelyTv\OpenFlight\Flights\Infrastructure;
 
 
 use CodelyTv\OpenFlight\Flights\Domain\Flight;
 use CodelyTv\OpenFlight\Flights\Domain\FlightRepository;
-use CodelyTv\OpenFlight\Users\Domain\User;
-use CodelyTv\OpenFlight\Users\Domain\UserRepository;
+use CodelyTv\OpenFlight\Flights\Domain\InvalidDepartureDate;
 use CodelyTv\Shared\Infrastructure\Persistence\Mysql;
 
 
@@ -23,17 +22,16 @@ final class MysqlFlightRepository implements FlightRepository
     {
         $sql = 'INSERT INTO flight VALUES(:id, :origin, :destination, :flightHours, :price, :currency, :departureDate, :aircraft, :airline)';
         $statement = $this->mysql->PDO()->prepare($sql);
-        $statement->bindValue(':id', $flight->ID()->value());
+        $statement->bindValue(':id', $flight->getId()->value());
         $statement->bindValue(':origin', $flight->getOrigin());
         $statement->bindValue(':destination', $flight->getDestination());
         $statement->bindValue(':flightHours', $flight->getFlightHours());
         $statement->bindValue(':price', $flight->getPrice());
         $statement->bindValue(':currency', $flight->getCurrency());
-        $statement->bindValue(':departureDate', $flight->getDepartureDate());
+        $statement->bindValue(':departureDate', Flight::convertDepartureDateToString($flight->getDepartureDate()));
         $statement->bindValue(':aircraft', $flight->getAircraft());
         $statement->bindValue(':airline', $flight->getAirline());
         $statement->execute();
     }
-
 
 }
