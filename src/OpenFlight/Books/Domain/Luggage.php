8aba621878c6e29a0ce323621b9aa348db4c3acf
Enrique Torrents Cuevas <80000917+EnriqueTorrents@users.noreply.github.com>
2021-05-09 22:07:59 +0200

    Luggage domain class

---
+++ src/OpenFlight/Books/Domain/Luggage.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace CodelyTv\OpenFlight\Books\Domain;
+
+
+use CodelyTv\Shared\Domain\ValueObject\Uuid;
+
+class Luggage
+{
+
+    private Uuid $id;
+    private string $type;
+    private WeightValueObject $weight;
+    private Uuid $bookId;
+
+    public function __construct(Uuid $id, string $type, WeightValueObject $weight, Uuid $bookId)
+    {
+        $this->id = $id;
+        $this->type = $type;
+        $this->weight = $weight;
+        $this->bookId = $bookId;
+    }
+
+    public static function createLuggage(Uuid $id, string $type, WeightValueObject $weight, Uuid $bookId): Luggage
+    {
+        self::validateType($type);
+        self::validateBook($bookId);
+
+        return new self($id, $type, $weight, $bookId);
+    }
+
+    private static function validateType(string $type)
+    {
+        if ($type === "") {
+            throw new InvalidLuggageType();
+        }
+    }
+
+    private static function validateBook(Uuid $bookId)
+    {
+        if ($bookId === null) {
+            throw new EmptyBook();
+        }
+    }
+
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getWeight(): WeightValueObject
+    {
+        return $this->weight;
+    }
+
+    public function getBookId(): Uuid
+    {
+        return $this->bookId;
+    }
+
+}
