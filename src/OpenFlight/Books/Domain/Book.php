e1000256f5077c2408f235c4bfdf9747f78dffe9
Enrique Torrents Cuevas <80000917+EnriqueTorrents@users.noreply.github.com>
2021-05-09 22:07:49 +0200

    Book Domain class

--- src/OpenFlight/Books/Domain/Book.php
+++ src/OpenFlight/Books/Domain/Book.php
@@ -6,12 +6,13 @@ namespace CodelyTv\OpenFlight\Books\Domain;
 
 use CodelyTv\Shared\Domain\Aggregate\AggregateRoot;
 use CodelyTv\Shared\Domain\ValueObject\PriceValueObject;
-use CodelyTv\Shared\Domain\ValueObject\SeatValueObject;
 use CodelyTv\Shared\Domain\ValueObject\Uuid;
 use DateTime;
 
 class Book extends AggregateRoot
 {
+
+    const buyDateFormat = 'Y-m-d H:i:s';
 
     private Uuid $id;
     private DateTime $buyDate;
@@ -39,16 +40,92 @@ class Book extends AggregateRoot
         $this->luggage = $luggage;
     }
 
-    public static function CreateBook(Uuid $id, DateTime $buyDate, SeatValueObject $seat, PriceValueObject $price, Uuid $flightId, Uuid $userId, Luggage $luggage): Book
-    {
+    public static function CreateBook(
+        Uuid $id,
+        DateTime $buyDate,
+        SeatValueObject $seat,
+        PriceValueObject $price,
+        Uuid $flightId,
+        Uuid $userId,
+        Luggage $luggage
+    ): Book {
         self::validateBuyDate($buyDate);
-        self::validateSeat($seat);
-        self::validatePrice($price);
         self::validateFlightId($flightId);
         self::validateUserId($userId);
-        self::validateLuggage($luggage);
 
         return new self($id, $buyDate, $seat, $price, $flightId, $userId, $luggage);
     }
+
+    private static function validateBuyDate(DateTime $buyDate)
+    {
+        if ($buyDate === null) {
+            throw new EmptyBuyDate();
+        }
+
+        if ($buyDate < new DateTime('NOW')) {
+            throw new InvalidLuggageType();
+        }
+    }
+
+    private static function validateFlightId(Uuid $flightId)
+    {
+        if ($flightId === null) {
+            throw new EmptyFlight();
+        }
+    }
+
+    private static function validateUserId(Uuid $userId)
+    {
+        if ($userId === null) {
+            throw new EmptyBook();
+        }
+    }
+
+
+    public static function convertBuyDateToDatetime(string $buyDate): DateTime
+    {
+        return DateTime::createFromFormat(self::buyDateFormat, $buyDate);
+    }
+
+    public static function convertBuyDateToString(DateTime $buyDate): string
+    {
+        return $buyDate->format(self::buyDateFormat);
+    }
+
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function getBuyDate(): DateTime
+    {
+        return $this->buyDate;
+    }
+
+    public function getSeat(): SeatValueObject
+    {
+        return $this->seat;
+    }
+
+    public function getPrice(): PriceValueObject
+    {
+        return $this->price;
+    }
+
+    public function getFlightId(): Uuid
+    {
+        return $this->flightId;
+    }
+
+    public function getUserId(): Uuid
+    {
+        return $this->userId;
+    }
+
+    public function getLuggage(): Luggage
+    {
+        return $this->luggage;
+    }
+
 
 }
