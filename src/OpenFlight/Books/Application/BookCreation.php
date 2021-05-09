7e4136140e1fa41b9c0bd7a5510f44b25675b1b5
Enrique Torrents Cuevas <80000917+EnriqueTorrents@users.noreply.github.com>
2021-05-09 22:08:49 +0200

    Book Application

---
+++ src/OpenFlight/Books/Application/BookCreation.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace CodelyTv\OpenFlight\Books\Application;
+
+
+use CodelyTv\OpenFlight\Books\Domain\Book;
+use CodelyTv\OpenFlight\Books\Domain\BookRepository;
+use CodelyTv\OpenFlight\Books\Domain\Luggage;
+use CodelyTv\OpenFlight\Books\Domain\SeatValueObject;
+use CodelyTv\OpenFlight\Books\Domain\WeightValueObject;
+use CodelyTv\Shared\Domain\ValueObject\PriceValueObject;
+use CodelyTv\Shared\Domain\ValueObject\Uuid;
+
+
+class BookCreation
+{
+
+    public function __construct(private BookRepository $repository)
+    {
+    }
+
+    public function __invoke(
+        string $id,
+        string $buyDate,
+        string $numberSeat,
+        string $letterSeat,
+        string $classSeat,
+        string $valuePrice,
+        string $currencyPrice,
+        string $flightId,
+        string $userId,
+        string $luggageType,
+        string $luggageWeightNumber,
+        string $luggageWightUnit
+    ): void {
+        $bookUuId = new Uuid($id);
+        $flightUuId = new Uuid($flightId);
+        $userUuId = new Uuid($userId);
+
+        $book = Book::CreateBook(
+            $bookUuId,
+            Book::convertBuyDateToDatetime($buyDate),
+            SeatValueObject::createSeat(intval($numberSeat), $letterSeat, $classSeat),
+            PriceValueObject::createPrice(intval($valuePrice), $currencyPrice),
+            $flightUuId,
+            $userUuId,
+            Luggage::createLuggage(
+                Uuid::random(),
+                $luggageType,
+                WeightValueObject::createWeight(intval($luggageWeightNumber), $luggageWightUnit),
+                $bookUuId
+            )
+        );
+
+        $this->repository->create($book);
+    }
+
+}
